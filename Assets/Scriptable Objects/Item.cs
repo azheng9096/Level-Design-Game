b9310bcf4a944455696c5daee71bbd501f67759d
Anna Zheng <26550446+azheng9096@users.noreply.github.com>
2023-02-25 18:57:11 -0500

    inventory system and interactable pick up items

---
+++ Assets/Scriptable Objects/Item.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Item", order = 1)]
+
+public class Item : ScriptableObject
+{
+    public string itemName;
+    public Sprite sprite;
+
+    public AudioClip pickUpAudio;
+
+    public Sprite detailSprite;
+    public string description;
+}
